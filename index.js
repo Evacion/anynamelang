585222afd1fd1a6d9378717311c8d97a147fa715
Ray James Gonzales <54382521+EvacionSaraak@users.noreply.github.com>
2024-02-22 19:09:17 +0800

    The script file is finally detectable

--- index.js
+++ index.js
@@ -1,31 +1,40 @@
-import { getServants } from "unique.js";
 
+const express = require("express")
+const app = express("./unique.js")
+const port = 8000
 const http = require('http')
 const fs = require('fs')
 
-const server = http.createServer((req, res) => {
-    console.log(req.url)
-    res.setHeader("Content-Type", "text/html")
+app.listen(port, () => { console.log(`Server Conencted to Port ${port}`) })
+app.get("/", (req, res) => { return res.sendFile(__dirname + "/index.html") })
+app.get("/error", (req, res) => { return res.sendFile(__dirname + "/error.html") })
+app.get("/about", (req, res) => { return res.sendFile(__dirname + "/about.html") })
+app.get("/unique", (req, res) => { return res.sendFile(__dirname + "/unique.html") })
+app.get("/unique.js", (req, res) => { return res.sendFile(__dirname + "/unique.js") })
+
+// const server = http.createServer((req, res) => {
+//     console.log(req.url)
+//     res.setHeader("Content-Type", "text/html")
     
-    let myurl = ""
-    if (req.url == "/") { 
-        myurl += 'index.html' 
-    } else if (req.url == "/about") { 
-        myurl += 'about.html' 
-    } else if (req.url == "/unique") { 
-        myurl += 'unique.html' 
-    } else { 
-        myurl += 'error.html' 
-    }
+//     let myurl = ""
+//     if (req.url == "/") { 
+//         myurl += 'index.html' 
+//     } else if (req.url == "/about") { 
+//         myurl += 'about.html' 
+//     } else if (req.url == "/unique") { 
+//         myurl += 'unique.html' 
+//     } else { 
+//         myurl += 'error.html' 
+//     }
 
-    fs.readFile(myurl, (err, data) => {
-        if (err) { console.log(err) } else {
-            res.write(data)
-            res.end()
-        }
-    })
-})
+//     fs.readFile(myurl, (err, data) => {
+//         if (err) { console.log(err) } else {
+//             res.write(data)
+//             res.end()
+//         }
+//     })
+// })
 
-server.listen(8000, 'localhost', () => {
-    console.log("[NAVI]: Hey, Listen.")    
-})
+// server.listen(8000, 'localhost', () => {
+//     console.log("[NAVI]: Hey, Listen.")    
+// })
--- unique.js
+++ unique.js
@@ -1,3 +1,6 @@
+const searchButton = document.getElementById("searchButton")
+const sortInput = document.getElementById("sortInput")
+
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
@@ -5,10 +8,9 @@ function capitalizeFirstLetter(string) {
 function getServants(sortName) {
     // const apiUrl = "https://api.atlasacademy.io/export/NA/nice_servant.json"
     const apiUrl = "https://api.atlasacademy.io/export/JP/nice_servant_lang_en.json"
-    let tableHeaders = ["Collection No.", "Image", "Servant Class", "Servant Name"]
     let tableData = []
 
-    console.log(`[${mymodule.fullname}] Fetching Data for ${sortName}...`)
+    console.log(`Fetching Data for ${sortName}...`)
     //let servantDataRaw = 
     fetch(apiUrl).then(response => {
         if (!response.ok) {throw new Error("Network has no response!!!");}
@@ -17,12 +19,10 @@ function getServants(sortName) {
         data.forEach(servant => {
             if (servant.name.toUpperCase().includes(sortName.toUpperCase())) {
                 var servantData = `[${servant.collectionNo}][${capitalizeFirstLetter(servant.className)}] ${servant.name}:`;
-                console.log(servantData); // ${servant.originalName}
-                
+                // console.log(servantData); // ${servant.originalName}
                 rawData = [servant.collectionNo, servant.extraAssets.faces.ascension[3] , servant.className, servant.name]
-                rawData.forEach(dt => { tableData.append(dt) })
-                
-                console.log(rawData);
+                rawData.forEach(dt => { tableData.push(dt) })
+                // console.log(rawData)
                 // var skillNames = [];
                 // servant.skills.forEach(skill => {
                 //     var skillTemp = `${skill.name}`;
@@ -38,9 +38,29 @@ function getServants(sortName) {
             }
         });
     }).catch(error => {console.log("Unexpected Error Encountered!", error)});
+    if (tableData.length > 0) { createTable(tableData) }
     return tableData;
 }
 
-export default {
-    getServants, 
+function createTable(tableData) {
+    let rawHTML = ""
+    let tableHeaders = ["Collection No.", "Image", "Servant Class", "Servant Name"]
+    //make table
+    rawHTML+="<table>"
+    rawHTML+="<tr>"
+    tableHeaders.forEach(th => {rawHTML+=`<th>${th}</th>`})
+    rawHTML+="</tr>"
+    for (let x=0; x<tableData.length; x++) {
+        rawHTML+="<tr>"
+        for (let y=0; y<tableHeaders.length; y++) {
+            rawHTML+=`<td>${tableData[x++]}</td>`
+        }
+        rawHTML+="</tr>"
+    }
+    rawHTML+="</table>"
+    console.log(rawHTML)
+    tableLoc.write(rawHTML)
 }
+
+searchButton.addEventListener('click', getServants(sortInput.value))
+// sortInput.addEventListener('change', function() { console.log(sortInput.value) })
