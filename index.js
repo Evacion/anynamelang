fadd96681a59ff6d18947256a40d31b4f85c2683
Saraak <rayjamesg@gmail.com>
2024-03-18 18:36:27 +0800

    Updated from Html to Ejs (functionally the same but has inline variables and can pass them via the index)

--- index.js
+++ index.js
@@ -4,19 +4,14 @@ const app = express()
 const port = 8000
 const http = require('http')
 const fs = require('fs')
+// app.engine('html', require('ejs').renderFile)
+// app.set('view engine', 'html')
 
 app.listen(port, () => { 
     console.log(`Server Connected`)
     console.log(`Port: ${port}`)
     console.log(`Directory: ${__dirname}`) 
 })
-app.get("/", (req, res) => { return res.sendFile(__dirname + "/home.html") })
-app.get("/home", (req, res) => { return res.redirect('/') })
-app.get("/about", (req, res) => { return res.sendFile(__dirname + "/about.html") })
-app.get("/aboutus", (req, res) => { return res.redirect('/about') })
-app.get("/unique", (req, res) => { return res.sendFile(__dirname + "/unique.html") })
-app.get("/portfolio", (req, res) => { return res.sendFile(__dirname + "/portfolio.html") })
-app.get("/navbar.html", (req, res) => { return res.sendFile(__dirname + "/navbar.html") })
 
 app.use((req, res, next) => {
     console.log("\nRequest Made")
@@ -25,6 +20,34 @@ app.use((req, res, next) => {
     console.log(`Method: ${req.method}`)
     next()
 })
+
+app.get("/", (req, res) => { 
+    res.render('home.ejs', {scaredBtn: "Hello", secretBtn: "Hey. It's me, ya boi, with a reminder"})
+    // return res.sendFile(__dirname + "/views/home.html") 
+})
+app.get("/home", (req, res) => { 
+    return res.redirect('/') 
+})
+app.get("/about", (req, res) => { 
+    res.render('about.ejs')
+    // return res.sendFile(__dirname + "/about.html") 
+})
+app.get("/aboutus", (req, res) => { 
+    return res.redirect('/about') 
+})
+app.get("/unique", (req, res) => { 
+    res.render('unique.ejs')
+    // return res.sendFile(__dirname + "/unique.html") 
+})
+app.get("/portfolio", (req, res) => { 
+    res.render('portfolio.ejs')
+    // return res.sendFile(__dirname + "/portfolio.html") 
+})
+app.get("/navbar.html", (req, res) => { 
+    res.render('navbar.ejs')
+    // return res.sendFile(__dirname + "/navbar.html") 
+})
+
 
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/css', express.static(__dirname + '/node_modules/datatables.net-bs5/css'));
